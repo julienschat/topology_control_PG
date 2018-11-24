4eb00d55c043ceb4dcee4c969e496086976b90e1
simonseyock <simonseyock@googlemail.com>
2018-11-24 16:52:10 +0100

    Model

--- src/Model/Graph.java
+++ src/Model/Graph.java
@@ -1,4 +1,32 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Graph {
+    public List<Node> nodeList = new ArrayList<>();
+    public List<Edge> edgeList = new LinkedList<>();
+
+    public Graph() { }
+
+    public void insertNode(Node n) {
+        this.nodeList.add(n);
+    }
+
+    public void connectNodes(Node a, Node b) {
+        Edge edge = new Edge(a, b);
+        a.edgeList.add(edge);
+        b.edgeList.add(edge);
+        this.edgeList.add(edge);
+    }
+
+    public Graph cloneGraphWithoutEdges() {
+        Graph newGraph = new Graph();
+        newGraph.nodeList = this.nodeList.stream()
+                .map(Node::cloneWithoutEdges)
+                .collect(Collectors.toList());
+        return newGraph;
+    }
 }
---
+++ src/Model/Node.java
@@ -0,0 +1,29 @@
+package Model;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Node {
+    public double x;
+    public double y;
+    public double radius;
+
+    public List<Edge> edgeList = new LinkedList<>();
+
+    public Node(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public List<Node> getNeighbours() {
+        return edgeList.stream()
+                .map(e -> e.getNeighbourOf(this))
+                .collect(Collectors.toList());
+    }
+
+    public Node cloneWithoutEdges() {
+        return new Node(this.x, this.y, this.radius);
+    }
+}
