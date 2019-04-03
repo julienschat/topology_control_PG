1eb536891d2c4b4e7e3c6b876ef474a53b1aceab
Julien <julienschat@web.de>
2019-04-03 19:53:20 +0200

    Introduce calculation of last edges

--- src/Controller/AlgorithmController.java
+++ src/Controller/AlgorithmController.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import Model.AlgorithmState;
+import Model.Edge;
+import Model.Node;
 
 abstract public class AlgorithmController {
 
@@ -19,6 +21,33 @@ abstract public class AlgorithmController {
 
     public Model.AlgorithmState back(Model.AlgorithmState state) {
         return state.previous == null ? state : state.previous;
+    }
+
+    public void calculateFinishedNetwork(Model.AlgorithmState state){
+        Edge[] maxEdgePerNode = new Edge[state.origin.nodeList.size()];
+        for(Edge edge : state.edgesChosen){
+            if(maxEdgePerNode[edge.left.id] !=null){
+                if(edge.getLength()>maxEdgePerNode[edge.left.id].getLength()) {
+                    maxEdgePerNode[edge.left.id] = edge;
+                }
+            }else{
+                maxEdgePerNode[edge.left.id] = edge;
+            }
+
+            if(maxEdgePerNode[edge.right.id] !=null){
+                if(edge.getLength()>maxEdgePerNode[edge.right.id].getLength()) {
+                    maxEdgePerNode[edge.right.id] = edge;
+                }
+            }else{
+                maxEdgePerNode[edge.right.id] = edge;
+            }
+        }
+        state.edgesChosen.clear();
+        for(Edge edge : state.origin.edgeList){
+            if((maxEdgePerNode[edge.left.id]!=null&&maxEdgePerNode[edge.left.id].getLength()>=edge.getLength())&&(maxEdgePerNode[edge.right.id]!=null&&maxEdgePerNode[edge.right.id].getLength()>=edge.getLength())){
+                state.edgesChosen.add(edge);
+            }
+        }
     }
 
     protected abstract Model.AlgorithmState processState(Model.AlgorithmState algorithmState);
