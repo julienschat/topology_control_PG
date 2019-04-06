628e7186b52fc2239ae19747e2a105e4da0178e9
simonseyock <simonseyock@googlemail.com>
2019-04-06 10:32:58 +0200

    other radii preselection

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
         try {
             Graph currentGraph = Graph.readFile("graph1.txt");
             editorForm.currentGraph = currentGraph;
-            editorForm.graphDrawer.draw(currentGraph, false,false, false);
+            editorForm.graphDrawer.draw(currentGraph, true,false, false);
         }catch(IOException ex){
             System.out.println("Could not load graph");
         }
